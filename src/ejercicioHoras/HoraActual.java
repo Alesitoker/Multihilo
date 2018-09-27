230ac27191f630d3ab73f7bf00edb134ce1165cc
Alejandro <alesitonightray@gmail.com>
2018-09-27 09:24:36 +0200

    Tiempo transcurrido

--- src/ejercicioHoras/HoraActual.java
+++
@@ -1,8 +0,0 @@
-package ejercicioHoras;
-
-public class HoraActual implements Runnable {
-    @Override
-    public void run() {
-
-    }
-}
--- src/ejercicioHoras/Main.java
+++ src/ejercicioHoras/Main.java
@@ -2,6 +2,14 @@ package ejercicioHoras;
 
 public class Main {
     public static void main(String[] args) {
-        
+       Thread t = new Thread(new TimeElapsed());
+       t.start();
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Han pasado 20 segundos");
     }
 }
